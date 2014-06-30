5f0333880df6c7d2ca33ff70aa324f49bfd218cb
Zena Hirsch <hirsch.zena@gmail.com>
2014-06-29 20:45:20 -0400

    support multiple wafflers on a single page, add readme

---
+++ README.md
@@ -0,0 +1,30 @@
+# Waffler jQuery Plugin
+
+## Demo
+
+You can see a live demo of this plugin [here](http://poblanoicecube.com/waffler-demo).
+
+## Example Usage
+
+### HTML
+
+```html
+<ul class="waffle">
+    <li class="red">Item 1</li>
+    <li class="orange">Item 2</li>
+    <li class="yellow">Item 3</li>
+    <li class="green">Item 4</li>
+    <li class="blue">Item 5</li>
+    <li class="purple">Item 6</li>
+</ul>
+```
+
+### jQuery
+
+```js
+$(document).waffler();
+```
+
+## License
+
+This plugin is available under [the MIT license](http://mths.be/mit).
--- jquery.waffler.js
+++ jquery.waffler.js
@@ -1,57 +1,41 @@
 ;(function($) {
 
     $.fn.waffler = function (options) {
-        var $waffle = $('#waffle'),
-            $waffleItems = $('#waffle li'),
-            $waffleItem,
-            $hoverItem,
-            dragging = false;
+        var $waffles = $('.waffle'),
+            $items = $waffles.find('li'),
+            dragging = false,
+            $waffle,
+            $item;
 
-        $waffleItems.mousedown(function () {
-            $waffleItem = $(this);
-            $waffleItem.css('opacity', '0.5');
+        $items.mousedown(function () {
+            $item = $(this);
+            $waffle = $item.parent();
+            $items.addClass('transparent');
+            $item.addClass('moving');
             dragging = true;
         });
 
-        $(document).mousemove(function (e) {
-            var yPos = e.clientY,
-                yOffset = e.offsetY;
-
-            console.log('yPos', yPos);
-
-            if (dragging) {
-                $waffleItem.css({
-                    'position': 'relative',
-                    'left': 0,
-                    'top': 10
-                });
-            }
-        });
-
-        $waffleItems.mousemove(function (e) {
-            var yPos = e.offsetY,
-                $hoverItem = $(this),
+        $items.mousemove(function (e) {
+            var $hoverItem = $(this),
+                overTop = false,
+                overBottom = false,
                 hoverItemHeight = $hoverItem.outerHeight(),
-                hoverTop = false,
-                hoverBottom = false,
-                $placeholder = $('li.placeholder');
+                yPos = e.offsetY;
 
-            yPos < (hoverItemHeight / 2) ? hoverTop = true : hoverBottom = true;
+            yPos < (hoverItemHeight / 2) ? overTop = true : overBottom = true;
 
-            if (dragging && hoverTop) {
-                $waffleItem.insertBefore($hoverItem);
+            if (dragging && overTop) {
+                $item.insertBefore($hoverItem);
             }
 
-            if (dragging && hoverBottom) {
-                $waffleItem.insertAfter($hoverItem);
+            if (dragging && overBottom) {
+                $item.insertAfter($hoverItem);
             }
         });
 
         $(document).mouseup(function () {
-            $waffleItem.css({
-                'opacity': '1',
-                'position': 'static'
-            });
+            $items.removeClass('transparent');
+            $item.removeClass('moving');
             dragging = false;
         });
 
